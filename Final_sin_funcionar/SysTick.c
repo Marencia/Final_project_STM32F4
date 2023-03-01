5a04be9baba52c7b71979401eb29d40cc0d2a0a5
Marencia <91967186+Marencia@users.noreply.github.com>
2023-03-01 05:19:47 -0300

    Create SysTick.c

---
+++ Final_sin_funcionar/SysTick.c
@@ -0,0 +1,30 @@
+/*
+ * Nombre: SysTick.c
+ * Autor: Eduardo L. Blotta y Flor :D
+ * Comentarios: Manejo de System Tick
+ * Año 2021
+ */
+#include "SysTick.h"
+
+// ----------------------------------------------------------------------------
+// ----- SysTick_Handler() ----------------------------------------------------
+// ----------------------------------------------------------------------------
+ __IO uint32_t TimeRecBase = 0;  /* Time Recording base variable */
+extern __IO uint32_t CmdIndex;
+
+
+void SysTick_Handler (void)
+{
+	HAL_IncTick();	//Incrementa Tick del sistema (usado por algunas funciones de HAL Driver)
+	SysTickHook (); // Función para colgarse de la interrupción del System Tick
+	/* Test on the command: Recording */
+	  if (CmdIndex == 1)
+	  {
+	    /* Increments the time recording base variable */
+	    TimeRecBase ++;
+	  }
+}
+
+void SysTick_Init(uint16_t Frec_Ticks_Hz){
+	SysTick_Config (SystemCoreClock / Frec_Ticks_Hz);
+}
